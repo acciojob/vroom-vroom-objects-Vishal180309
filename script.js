7ae2b2da307ab2922e69831a051381cff3915581
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-10 09:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-class Car {
-    constructor(make, model) {
-        this.make = make;
-        this.model = model;
-    }
-
-    getMakeModel() {
-        return `${this.make} ${this.model}`;
-    }
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
 }
 
-class SportsCar extends Car {
-    constructor(make, model, topSpeed) {
-        super(make, model);
-        this.topSpeed = topSpeed;
-    }
+Car.prototype.getMakeModel = function() {
+    return this.make + ' ' + this.model;
+};
 
-    getTopSpeed() {
-        return this.topSpeed;
-    }
+function SportsCar(make, model, topSpeed) {
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
+
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
 document.getElementById('create-car-button').addEventListener('click', function() {
     const car = new Car('Toyota', 'Corolla');
-    document.getElementById('result').innerHTML = `Car: ${car.getMakeModel()}`;
+    document.getElementById('result').innerHTML = 'Car: ' + car.getMakeModel();
 });
 
 document.getElementById('create-sports-car-button').addEventListener('click', function() {
     const sportsCar = new SportsCar('Ferrari', 'Testarossa', 200);
-    document.getElementById('result').innerHTML = `Sports Car: ${sportsCar.getMakeModel()} - Top Speed: ${sportsCar.getTopSpeed()}mph`;
+    document.getElementById('result').innerHTML = 'Sports Car: ' + sportsCar.getMakeModel() + ' - Top Speed: ' + sportsCar.getTopSpeed() + 'mph';
 });
